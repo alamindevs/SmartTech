0bbb6c0c841b30bf026187da0d44fbe331aad181
MD.Al-Amin Hawladar <alamingeneral455@gmail.com>
2020-03-10 20:34:28 +0600

    laravel helper package install (composer require laravel/helpers) & user migrateion file collumn setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,15 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('phone')->nullable()->unique();
+            $table->string('address')->nullable();
+            $table->string('image')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
