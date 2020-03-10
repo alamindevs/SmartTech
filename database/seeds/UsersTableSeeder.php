b29e6ba77b4c1605f1fbd2e41c414d19bca24176
MD.Al-Amin Hawladar <alamingeneral455@gmail.com>
2020-03-10 21:30:42 +0600

    users table git add & push

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::insert([
+        	[	
+        		'name' => 'MD.Al-Amin Hawladar',
+        		'email' => 'alamingeneral455@gmail.com',
+        		'username' => 'mdalmain',
+        		'phone' => '0172415312',
+        		'address' => 'Mirpur-14',
+        		'image' => Null,
+        		'email_verified_at' => Carbon::now()->toDateTimeString(),
+        		'password' => Hash::make('12345678'),
+        	],[
+        		'name' => 'MD.Al-Amin',
+        		'email' => 'almain@gmail.com',
+        		'username' => 'alamin',
+        		'phone' => '01876619765',
+        		'address' => 'Mirpur-14',
+        		'image' => Null,
+        		'email_verified_at' => Carbon::now()->toDateTimeString(),
+        		'password' => Hash::make('12345678'),
+        	]
+        ]);
+    }
+}
