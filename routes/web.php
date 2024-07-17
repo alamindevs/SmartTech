dd151391e2159594a93f756846edf3177a03f8a7
MD. Al-Amin Hawladar <60581249+alamindevs@users.noreply.github.com>
2024-07-17 16:53:03 +0600

    some chage

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    echo 'alamin';
     return view('welcome');
 });
 Route::group(['namespace' => 'Website'], function(){
